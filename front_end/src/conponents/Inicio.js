8b92cbeca2064cde0a0b25507ef51df3f801e774
robert <r.gabriel.f@outlook.com>
2023-07-31 17:24:00 -0400

    conocimos img

--- front_end/src/conponents/Inicio.js
+++ front_end/src/conponents/Inicio.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "../style/Inicio.css";
+import i from "../assests/img/fondo.png";
 export const Inicio = () => {
   return (
     <section className="container-inicio">
-      <div className="u">
-      <div className="inicio">
-        <h1>Servicios de desarrollo de software</h1>
+      <div className="contenido-inicio">
+        <div className="detalle-inicio">
+          <h1>Servicios de desarrollo de software</h1>
+        </div>
+        <div className="img-inicio">
+          <img src={i} id="img-fondo" alt="Descripción de la imagen"></img>
+        </div>
       </div>
-      <div className="otro">
-        <h1>Servicios de desarrollo de software</h1>
-      </div>
-      </div>
-      
     </section>
   );
 };
