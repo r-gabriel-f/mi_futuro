22fe3442795ccfd6de08cb965e353c5a6896ee39
robert <r.gabriel.f@outlook.com>
2023-07-31 17:08:38 -0400

    inciamos proyecto

---
+++ front_end/src/conponents/Inicio.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../style/Inicio.css";
+export const Inicio = () => {
+  return (
+    <section className="container-inicio">
+      <div className="u">
+      <div className="inicio">
+        <h1>Servicios de desarrollo de software</h1>
+      </div>
+      <div className="otro">
+        <h1>Servicios de desarrollo de software</h1>
+      </div>
+      </div>
+      
+    </section>
+  );
+};
