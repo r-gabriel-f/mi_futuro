95fb68c099303fa73378b1f00f00f7d7e2fb636a
robert <r.gabriel.f@outlook.com>
2023-11-20 15:24:47 -0400

    se corrigio el formulario

--- front_end/src/conponents/Contactos.js
+++ front_end/src/conponents/Contactos.js
@@ -53,108 +53,84 @@ export const Contactos = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full my-10">
         <div className="flex flex-col my-auto">
           <h2 className="text-center text-4xl font-serif text-black">
-            Contactanos
-          </h2>
-          <div className="mx-auto">
-            <div className="flex  mt-10 ">
-              <ion-icon
-                name="call-outline"
-                class="w-10 h-10 text-[#0F172A] mr-10"
-              ></ion-icon>
-              <p>+591 67440842</p>
-            </div>
-            <div className="flex  mt-10 ">
-              <ion-icon
-                name="mail-outline"
-                class="w-10 h-10 text-[#0F172A]  mr-10"
-              ></ion-icon>
-              <p>r.gabriel.f@outlook.com</p>
-            </div>
-            <div className="flex  mt-10 ">
-              <ion-icon
-                name="compass-outline"
-                class="w-10 h-10 text-[#0F172A] mr-10"
-              ></ion-icon>
-              <p>Av. Blanco galindo Km10</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-col my-auto">
-          <h2 className="text-center text-4xl font-serif text-black">
             Formulario
           </h2>
 
           <form onSubmit={handleSubmit} className="mx-10 mt-10">
-            <div className="mb-6">
-              <label
-                htmlFor="nombre"
-                className="block mb-2 font-medium text-black"
-              >
-                Nombres y Apellidos
-              </label>
-              <input
-                type="nombre"
-                id="nombre"
-                value={nombre}
-                onChange={(e) => setNombre(e.target.value)}
-                className="text-sm w-full p-2.5 bg-[#0F172A] rounded-md text-[#DAF7F4]"
-                required
-              ></input>
+            <div className="grid grid-cols-2 gap-4">
+              <div className="mb-6">
+                <label
+                  htmlFor="nombre"
+                  className="block mb-2 font-medium text-black"
+                >
+                  Nombres y Apellidos
+                </label>
+                <input
+                  type="nombre"
+                  id="nombre"
+                  value={nombre}
+                  onChange={(e) => setNombre(e.target.value)}
+                  className="text-sm w-full p-2.5 bg-[#0F172A] rounded-md text-[#DAF7F4]"
+                  required
+                ></input>
+              </div>
+              <div className="mb-6">
+                <label
+                  htmlFor="telefono"
+                  className="block mb-2 font-medium text-black"
+                >
+                  Telefono
+                </label>
+                <input
+                  type="tel"
+                  id="telefono"
+                  value={telefono}
+                  onChange={(e) => {
+                    const onlyNums = e.target.value
+                      .replace(/[^0-9]/g, "")
+                      .slice(0, 8);
+                    setTelefono(onlyNums);
+                  }}
+                  className="text-sm w-full p-2.5 bg-[#0F172A] rounded-md text-[#DAF7F4]"
+                  required
+                ></input>
+              </div>
             </div>
-            <div className="mb-6">
-              <label
-                htmlFor="email"
-                className="block mb-2 font-medium text-black"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                value={from}
-                onChange={(e) => setFrom(e.target.value)}
-                className="text-sm w-full p-2.5 bg-[#0F172A] rounded-md text-[#DAF7F4]"
-                required
-              ></input>
+            <div className="grid grid-cols-2 gap-4">
+              <div className="mb-6">
+                <label
+                  htmlFor="email"
+                  className="block mb-2 font-medium text-black"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  value={from}
+                  onChange={(e) => setFrom(e.target.value)}
+                  className="text-sm w-full p-2.5 bg-[#0F172A] rounded-md text-[#DAF7F4]"
+                  required
+                ></input>
+              </div>
+              <div className="mb-6">
+                <label
+                  htmlFor="asunto"
+                  className="block mb-2 font-medium text-black"
+                >
+                  Asusto
+                </label>
+                <input
+                  type="asunto"
+                  id="asunto"
+                  value={subject}
+                  onChange={(e) => setSubject(e.target.value)}
+                  className="text-sm w-full p-2.5 bg-[#0F172A] rounded-md text-[#DAF7F4]"
+                  required
+                ></input>
+              </div>
             </div>
-            <div className="mb-6">
-              <label
-                htmlFor="telefono"
-                className="block mb-2 font-medium text-black"
-              >
-                Telefono
-              </label>
-              <input
-                type="tel"
-                id="telefono"
-                value={telefono}
-                onChange={(e) => {
-                  const onlyNums = e.target.value
-                    .replace(/[^0-9]/g, "")
-                    .slice(0, 8);
-                  setTelefono(onlyNums);
-                }}
-                className="text-sm w-full p-2.5 bg-[#0F172A] rounded-md text-[#DAF7F4]"
-                required
-              ></input>
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="asunto"
-                className="block mb-2 font-medium text-black"
-              >
-                Asusto
-              </label>
-              <input
-                type="asunto"
-                id="asunto"
-                value={subject}
-                onChange={(e) => setSubject(e.target.value)}
-                className="text-sm w-full p-2.5 bg-[#0F172A] rounded-md text-[#DAF7F4]"
-                required
-              ></input>
-            </div>
+
             <div className="mb-6">
               <label
                 htmlFor="mensaje"
@@ -181,6 +157,34 @@ export const Contactos = () => {
               </button>
             </div>
           </form>
+        </div>
+        <div className="flex flex-col my-auto">
+          <h2 className="text-center text-4xl font-serif text-black">
+            Contactanos
+          </h2>
+          <div className="mx-auto">
+            <div className="flex  mt-10 ">
+              <ion-icon
+                name="call-outline"
+                class="w-10 h-10 text-[#0F172A] mr-10"
+              ></ion-icon>
+              <p>+591 67440842</p>
+            </div>
+            <div className="flex  mt-10 ">
+              <ion-icon
+                name="mail-outline"
+                class="w-10 h-10 text-[#0F172A]  mr-10"
+              ></ion-icon>
+              <p>r.gabriel.f@outlook.com</p>
+            </div>
+            <div className="flex  mt-10 ">
+              <ion-icon
+                name="compass-outline"
+                class="w-10 h-10 text-[#0F172A] mr-10"
+              ></ion-icon>
+              <p>Av. Blanco galindo Km10</p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="w-full h-screen">
