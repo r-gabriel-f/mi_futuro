42a26bbcbfe8579e30e35c2f9bf5171dea3ed064
robert <r.gabriel.f@outlook.com>
2023-11-22 10:21:38 -0400

    se modifico el final para el año actual

--- front_end/src/conponents/Final.js
+++ front_end/src/conponents/Final.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 export const Final = () => {
+  var fechaActual = new Date();
+  var añoActual = fechaActual.getFullYear();
   return (
     <section className="bg-[#0F172A] h-20">
       <div className="flex justify-center items-center h-full">
         <span className="text-zinc-50">
-          Copyright © 2023 FlowSoft, All Rights Reserved.
+          Copyright © {añoActual} FlowSoft, All Rights Reserved.
         </span>
       </div>
     </section>
