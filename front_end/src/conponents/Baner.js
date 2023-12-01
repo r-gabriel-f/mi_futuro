1b2b0f46ef820118a293c0dc1f35b00cb6fc01de
robert <r.gabriel.f@outlook.com>
2023-12-01 15:53:26 -0400

    se quito la opasidad

--- front_end/src/conponents/Baner.js
+++ front_end/src/conponents/Baner.js
@@ -10,7 +10,7 @@ export const Baner = () => {
   return (
     <section className="h-screen bg-baner">
       <div className="flex flex-col justify-center items-center h-full ">
-        <div className="bg-[#0F172A] opacity-70 border border-solid border-[#ffff] rounded p-10 text-center">
+        <div className="bg-[#0F172A] border border-solid border-[#ffff] rounded p-10 text-center">
           <h2 className="text-5xl text-[#DAF7F4]">FlowSoft</h2>
           <button className="border border-solid border-[#DAF7F4] text-[#DAF7F4] font-bold py-2 px-4 rounded-full mt-10" onClick={redirectToContactos}>
             Contactar
